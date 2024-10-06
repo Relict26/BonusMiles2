24c4df7167d983cc932de48db224a7df0ff6c82f
Relict <vostryakovkirill@gmail.com>
2024-10-06 21:35:47 +0400

    Initial commit

---
+++ src/BonusMiles.java
@@ -0,0 +1,6 @@
+public class BonusMiles {
+    public int calculate(int price) {
+        int miles = price / 20; // целочисленное деление для округления до целого числа
+        return miles;
+    }
+}
